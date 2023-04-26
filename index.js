f3b0fd02ffff5c9bc8768569feea8895becffd84
Francisco Lopez <fran.jlp26@gmail.com>
2023-04-25 18:16:20 -0600

    add counter

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-import express from 'express';
+import express from "express";
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Choo Choo! Welcome to your Express app 🚅');
-})
+let count = 0;
+
+app.get("/", (req, res) => {
+  count++;
+  res.send({'count': count});
+});
 
 app.get("/json", (req, res) => {
-    res.json({"Choo Choo": "Welcome to your Express app 🚅"});
-})
+  res.json({ "Choo Choo": "Welcome to your Express app 🚅" });
+});
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
