1a09482d2eb5276a6aec625fd0ff75ec38de19bd
Francisco Lopez <fran.jlp26@gmail.com>
2023-04-26 08:30:20 -0600

    test

--- index.js
+++ index.js
@@ -2,10 +2,25 @@ import express from "express";
 const app = express();
 
 let count = 0;
+const logs = [];
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   count++;
-  res.send({'count': count});
+  res.send(logs);
+});
+
+app.get("/reset", (req, res) => {
+  count = 0;
+  logs.length = 0;
+  res.send({ message: "clear" });
+});
+
+app.post("/", (req, res) => {
+  count++;
+  logs.push({ date: new Date(), body: req.body, headers: req.headers });
+  res.send({ test: "yes" });
 });
 
 app.get("/json", (req, res) => {
